6835a5a678a3fdec819ef6353b6bb94d734de1ed
noneiann <reyianntigley@gmail.com>
2025-05-12 16:41:03 +0800

    setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# RICEBLAST_ABM
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿// RiceBlast.cpp : Defines the entry point for the application.
+//
+#include "SDL3/SDL.h"
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	SDL_Log("Hello World!");
+	SDL_Log("I am Iann");
+	return 0;
+}
